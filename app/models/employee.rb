ea5a2c481ea9f5b3a6626882c907308e1832d4e2
Ankit Gautam <0agautam@gmail.com>
2021-03-05 16:52:16 +0530

    Self.search method added for Search field on Index page

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -9,4 +9,19 @@ class Employee < ApplicationRecord
 	#has_many :myequipments, dependent: :destroy
 	has_many :laptops, dependent: :destroy
 	has_many :accounts
+
+  def self.search(search)
+    if search 
+      found_employees = Employee.where("name ilike ?", "%#{search}%")
+
+      if found_employees
+        self.where(id: found_employees)
+ 
+      else
+        @employees = Employee.all
+      end
+    else
+      @employees = Employee.all
+    end
+  end
 end
